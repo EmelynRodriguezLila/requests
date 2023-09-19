408aad0b737c2ac6f3d25e32937f273a965f5e37
Emelyn Rodriguez Lila <emelynlila04li@gmail.com>
2023-09-19 12:49:35 -0600

    my first requests

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+
+const app = express()
+
+//http://localhost:3000/emelyn/123
+app.get('/:user/:password', (req, res) => {
+    const {user, password} = req.params
+    if(user === 'emelyn' && password === '123'){
+      res.json({msg: 'Inicio de sesion exitoso'})
+      return  
+    }
+    res.json({msg: 'Fallo en el usuario o contraseña'})
+}) 
+
+app.post('/', (req, res) => {
+    res.json({msg: 'Hello POST'})
+})
+
+app.put('/', (req, res) => {
+    res.json({msg: 'Hello PUT'})
+}) 
+
+app.patch('/', (req, res) => {
+    res.json({msg: 'Hello PATCH'})
+}) 
+
+app.delete('/', (req, res) => {
+    res.json({msg: 'Hello DELETE'})
+}) 
+
+/**
+ * 
+ * FUNCIONALIDAD
+ * 
+ */
+
+//http://localhost:3000/
+app.listen(3000, () => {
+    console.log('listening on port 3000')
+})
