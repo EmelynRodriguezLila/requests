4799ed26f146909e2ab9ad6fc9ed47393de5d74b
Emelyn Rodriguez Lila <emelynlila04li@gmail.com>
2023-09-20 12:48:52 -0600

    Query params

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 //http://localhost:3000/emelyn/123
-app.get('/:user/:password', (req, res) => {
+app.get('/:user/:password', (req, res) => {//Endpoint el conjunto del metodo http, la ruta y la funcionalidad
     res.json({msg: 'Hello Get'})
 }) 
 app.get('/:user/:password', (req, res) => {
@@ -14,6 +14,20 @@ app.get('/:user/:password', (req, res) => {
     }
     res.json({msg: 'Fallo en el usuario o contraseña'})
 }) 
+//OTRO METODO USANDO QUERY
+//http://localhost:3000/login?user=emelyn&password=123
+app.get('/login', (req, res) => {
+    const {user, password} = req.query
+    if(!user || !password){
+        res.json({msg: "You nedd to provide <user> and <password> params"})
+    }
+
+    if(user === 'emelyn' && password === '123'){
+      res.status(400).json({msg: 'Sign In succesfully'})
+      return  
+    }
+    res.status(404).json({msg: 'Wrong user or password'})
+})
 
 app.post('/', (req, res) => {
     res.json({msg: 'Hello POST'})
